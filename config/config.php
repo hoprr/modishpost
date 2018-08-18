c76b61b0956e13791a24d68c5cc2d74f22d42941
Yash <36479718+yashguptaz@users.noreply.github.com>
2018-08-18 12:49:05 +0530

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php 
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '*****');
+define('DB_NAME', 'blog');
+
+
+$site_description = "Modish Post creates professional blog posts on making a life worth living. Modish Post has helped countless people in making there life worth it!
+<br><br> 
+Or you can unproductively spend another 5 hours scrolling down your facebook or whatsapp feed if you don't want your life to be worth it.";
