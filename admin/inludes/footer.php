35f33dc7b603a70c0acf3fce050537f38730f8e5
Yash <36479718+yashguptaz@users.noreply.github.com>
2018-08-18 12:46:17 +0530

    Add files via upload

---
+++ admin/inludes/footer.php
@@ -0,0 +1,16 @@
+	<div style="height: 200px;" class="content"></div>
+ 	<script src="../js/stickynav.js"></script>
+ 	<script src="../js/bs.js"></script> 
+ 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-123213678-1"></script>
+	<script src="https://www.gstatic.com/firebasejs/5.3.0/firebase.js"></script>
+	<script src="../js/analysis.js"></script>
+	<script src="../js/medium-editor.js"></script>
+
+	<script>var editor = new MediumEditor('.editable');</script>
+
+	<script>
+		var elements = document.querySelectorAll('.editable'),
+    	editor = new MediumEditor(elements);
+	</script>
+</body>
+</html>
